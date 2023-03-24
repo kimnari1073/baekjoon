2ea2dd19c785e85dbc1d501f5c31844e7ed594b2
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2023-03-24 10:30:13 +0900

    [Unrated] Title: 합, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Unrated/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ct = Integer.parseInt(br.readLine());
+        int sum=0;
+        for(int i=1; i<=ct; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
