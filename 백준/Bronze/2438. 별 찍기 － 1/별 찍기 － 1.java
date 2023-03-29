0215e1ec06ac3b6887ceac38f09defb29d82f69d
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2023-03-29 17:05:50 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class Main{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=num;i++){
+            for(int k=1;k<=i;k++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
