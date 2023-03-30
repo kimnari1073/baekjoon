b9a703cb6e9feda23519696c2d0d8ef28390f4d0
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2023-03-30 16:43:42 +0900

    [Bronze V] Title: 개수 세기, Time: 132 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -3,14 +3,22 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 public class Main{
-    public static void main(String[]args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String []arr = new String[Integer.parseInt(br.readLine())];
-        arr = br.readLine().split(" ");
-        int count = 0;
-        String num = br.readLine();
+        private static int read() throws IOException{
+        int c,n = System.in.read() & 15;
+        while((c= System.in.read())>32)
+            n = (n <<3) + (n<<1) + (c&15);
+        if(c==13) System.in.read();
+        return n;
+    }
+    public static void main(String[]args) throws IOException{
+        int []arr = new int[read()];
         for(int i=0;i<arr.length;i++){
-            if(arr[i].equals(num)) count++;
+            arr[i] = read();
+        }
+        int count = 0;
+        int num = read();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==num) count++;
         }
         System.out.println(count);
     }
