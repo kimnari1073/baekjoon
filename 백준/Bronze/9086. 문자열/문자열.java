7e8278affe5fca2ce4288968b168dcc6e833aabe
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2023-04-02 15:33:52 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,16 +1,18 @@
-import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[]args)throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int ct = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<ct;i++){
-            String st = br.readLine();
-            sb.append(st.charAt(0)).append(st.charAt(st.length()-1)).append("\n");
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            String str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length()-1)).append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
+
     }
 }
