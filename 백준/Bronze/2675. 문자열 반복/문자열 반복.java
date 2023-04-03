8b9cef68edc759123061869571471625c8259028
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2023-04-03 17:33:33 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,25 +1,33 @@
-import java.io.IOException;
+/**/
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    public static void main(String[]args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputNum = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<inputNum;i++){
-            StringTokenizer tk= new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(tk.nextToken());
-            String str = tk.nextToken();
-            for(int k=0; k<str.length();k++){
-                for(int j=0;j<n;j++){
-                    sb.append(str.charAt(k));
-                }
-            }
-            if(i!=inputNum-1)
-                sb.append("\n");
-        }
-        System.out.println(sb);
-    }
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter print = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(input.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			String[] str = input.readLine().split(" ");
+			int R = Integer.parseInt(str[0]);
+			char[] ch = str[1].toCharArray();
+			
+			for(int j = 0; j < ch.length; j++) {
+				for(int k = 0; k < R; k++) {
+					print.write(ch[j]);
+				}
+			}
+			print.write("\n");
+		}
+		
+		print.flush();
+	}
+
 }
