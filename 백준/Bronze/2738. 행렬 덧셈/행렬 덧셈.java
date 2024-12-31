5cd28e5f9700cf801b552af7d9d05e25e3d77c58
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2024-12-31 10:22:31 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 212 ms, Memory: 18888 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,53 +1,43 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] sizeArr = br.readLine().split(" ");
-        int sizeA=Integer.parseInt(sizeArr[0]);
-        int sizeB=Integer.parseInt(sizeArr[1]);
+        int row=Integer.parseInt(sizeArr[0]);
+        int col=Integer.parseInt(sizeArr[1]);
         
-        int[][] arrA = new int[sizeA][sizeB];
-        int[][] arrB = new int[sizeA][sizeB];
+        int size = row * col;
+        int[] input = br.lines()
+            .flatMap(line->Arrays.stream(line.split("\\s+")))
+            .mapToInt(Integer::parseInt)
+            .toArray();
         
-        
-        for(int i=0;i<sizeA;i++){
-            String[] arr_String = br.readLine().split(" ");
-            for(int j=0;j<sizeB;j++){
-                arrA[i][j] = Integer.parseInt(arr_String[j]);
+        int[][] resultArr = new int[row][col];
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                resultArr[i][j] = input[count] +input[count+size];
+                count++;
             }
-        }
-        for(int i=0;i<sizeA;i++){
-            String[] arr_String = br.readLine().split(" ");
-            for(int j=0;j<sizeB;j++){
-                arrB[i][j] = Integer.parseInt(arr_String[j]);
-            }
-        }
-        
-        int[][] resultArr = new int[sizeA][sizeB];
-        for(int i=0;i<sizeA;i++){
-            for(int j=0;j<sizeB;j++){
-                resultArr[i][j]= arrA[i][j] + arrB[i][j];
-            }
+  
         }
         
-        for(int i=0;i<sizeA;i++){
-            for(int j=0;j<sizeB;j++){
-                System.out.print(resultArr[i][j]);
-                if(j+1==sizeB){
-                    System.out.print("\n");
-                }else{
-                    System.out.print(" ");
+       StringBuilder sb = new StringBuilder();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                sb.append(resultArr[i][j]);
+                if(j<col-1){
+                    sb.append(" ");
                 }
-                
             }
+            sb.append("\n");
         }
         
-        
-        
-
+        System.out.print(sb.toString());
     }
 }
